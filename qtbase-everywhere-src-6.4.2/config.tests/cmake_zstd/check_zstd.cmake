a858c933eacf014142ec7abb51700afa912c2842
NoTKanye23 <gajendranath025@gmail.com>
2026-05-19 19:49:23 +0530

    pipeline v5.1: library_tokens, set-difference vendoring, searchperpackage support

--- qtbase-everywhere-src-6.4.2/config.tests/cmake_zstd/check_zstd.cmake
+++
@@ -1,5 +0,0 @@
-file(ARCHIVE_CREATE
-    OUTPUT cmake_zstd.zstd
-    PATHS "${CMAKE_CURRENT_LIST_FILE}"
-    FORMAT raw
-    COMPRESSION Zstd)
--- qtbase-everywhere-src-6.4.2/src/widgets/doc/snippets/cmake-macros/examples.cmake
+++
@@ -1,5 +0,0 @@
-#! [qt_wrap_ui]
-set(SOURCES mainwindow.cpp main.cpp)
-qt_wrap_ui(SOURCES mainwindow.ui)
-add_executable(myapp ${SOURCES})
-#! [qt_wrap_ui]
